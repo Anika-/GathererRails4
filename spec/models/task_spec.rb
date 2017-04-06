ff0cf7b5e7fece7faa7df6d3a8e94886ced65d51
Ana Batista <ana@joyjet.com>
2017-04-06 15:33:42 -0300

    Created Project and Task objects

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Task do
+	
+	describe "initialization" do
+		task = Task.new
+
+	it "can distinguish a completed task" do
+		expect(task).not_to be_complete
+		task.mark_completed
+		expect(task).to be_complete
+	end
+	end
+end
